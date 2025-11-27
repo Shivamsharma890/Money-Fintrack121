f069179d68244b38f4c6669f93fd2971fb7ec8bf
Shivam Sharma <ss7614103@gmail.com>
2025-11-27 19:02:23 +0530

    Update user.py

--- backend/user.py
+++ backend/user.py
@@ -18,32 +18,6 @@ app = Flask(__name__)
 CORS(app)
 
 def get_connection():
-    #  # support both local .env names and Railway's default variable names
-    # host = os.getenv("host") or os.getenv("MYSQLHOST") or os.getenv("MYSQL_URL") or os.getenv("MYSQL_PUBLIC_URL")
-    # user = os.getenv("user") or os.getenv("MYSQLUSER") or os.getenv("MYSQL_USERNAME") or os.getenv("MYSQL_ROOT_USER")
-    # password = os.getenv("password") or os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_ROOT_PASSWORD") or os.getenv("MYSQL_ROOT_PASS")
-    # database = os.getenv("database") or os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE") or os.getenv("MYSQL_DATABASE_NAME")
-    # port_str = os.getenv("port") or os.getenv("MYSQLPORT") or os.getenv("MYSQL_PORT_NUMBER") or "3306"
-
-    # try:
-    #     port = int(port_str)
-    # except:
-    #     port = 3306
-
-    # # debug print so you can see what values are being picked up in logs
-    # print("DB CONNECT VALUES: host=", bool(host), " user=", bool(user), " db=", bool(database), " port=", port)
-
-    # return mysql.connector.connect(
-    #     host=host,
-    #     user=user,
-    #     password=password,
-    #     database=database,
-    #     port=port
-    # )
-    
-    
-    
-    
     return mysql.connector.connect(
         host=os.getenv("host"),
         user=os.getenv("MYSQLUSER"),
@@ -111,10 +85,6 @@ def get_users():
             except:
                 row["balance"] = 0
 
-        
-        
-        
-
         cursor.close()
         conn.close()
 
@@ -175,7 +145,6 @@ def login_user():
     except Exception as e:
         print("LOGIN ERROR:", e)
         return jsonify({"error": str(e)}), 500
-
 
 #----------------------balance--------------------------------
 @app.route("/balance", methods=["POST"])
@@ -274,16 +243,7 @@ def transfer():
         except Exception as sql_error:
           print("TRANSACTION INSERT ERROR:", sql_error)
           return jsonify({"error": "Insert failed", "details": str(sql_error)}), 500
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+               
         conn.commit()
 
         cursor.close()
@@ -304,7 +264,6 @@ RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 
 # Initialize-Razorpay-client
 client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET))
-
 
 # Create-Order
 @app.route("/create_order", methods=["POST"])
@@ -501,7 +460,6 @@ def edit_details():
         print("UPDATE ERROR:", e)
         return jsonify({"error": str(e)}), 500
     
-    
 #--------------------forget id-password----------------------
 @app.route("/forgot", methods=["POST"])
 def forgot_credentials():
@@ -554,7 +512,6 @@ def update_credentials():
     conn.close()
 
     return jsonify({"message": "Credentials updated successfully!"}), 200
-
 
 
 if __name__ == "__main__":
