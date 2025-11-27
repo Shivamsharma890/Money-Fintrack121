417745f14d22063e1ebddbe3098fcc0fa0ec6acd
Shivam Sharma <ss7614103@gmail.com>
2025-11-27 18:25:30 +0530

    commit all

--- backend/user.py
+++ backend/user.py
@@ -215,20 +215,6 @@ def transfer():
         password = data.get("password")
         action = data.get("action")
         amount = int(data.get("amount", 0))
-        # raw_amount = data.get("amount")
-
-        # # FIX: Validate amount properly
-        # if raw_amount is None or raw_amount == "" or raw_amount == "null":
-        #     return jsonify({"error": "Amount is missing"}), 400
-
-        # # FIX: Convert safely
-        # try:
-        #     amount = int(float(raw_amount))
-        # except:
-        #     return jsonify({"error": "Invalid amount format"}), 400
-        
-        
-        
 
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
--- frontend/src/Pages/Balance.jsx
+++ frontend/src/Pages/Balance.jsx
@@ -13,7 +13,7 @@ const Balance = () => {
     setStatus("checking");
 
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}balance`, {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/balance`, {
         userid: userID,
         password: password,
       });
